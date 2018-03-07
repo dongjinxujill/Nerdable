10622542824c31f02463e2db6f7d1038b68a246a
dongjinxujill <32142414+dongjinxujill@users.noreply.github.com>
2018-03-06 22:31:17 -0500

    ready to work on project backend

--- frontend/reducers/errors/errors_reducer.js
+++ frontend/reducers/errors/errors_reducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import sessionErrorsReducer from './session_errors_reducer';
+import sessionErrorsReducer from '../errors/session_errors_reducer';
 
 const errorsReducer = combineReducers({
   session: sessionErrorsReducer
