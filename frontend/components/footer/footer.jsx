48d7912cfe1057bcee5852fa74dae0b43f8e6538
dongjinxujill <32142414+dongjinxujill@users.noreply.github.com>
2018-03-06 19:27:53 -0500

    css dropdown button

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Footer = (props) => {
-  return {
-    <a href="https://github.com/dongjinxujill"></a>;
-  };
+  return <h1>footer</h1>;
 };
 
 export default Footer;
