8b2cad5062d55ef7cc15dd5d5f84094f6bd6b3a8
dongjinxujill <32142414+dongjinxujill@users.noreply.github.com>
2018-03-29 09:54:58 -0400

    fix github link, add linkedin

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,7 +4,8 @@ class Footer extends React.Component{
   render(){
     return (
       <ul id='footer-link'>
-        <i className="fab fa-github-square"><a href='https://github.com/dongjinxujill/TheRule'></a></i>
+        <a href="https://github.com/dongjinxujill/TheRule"><i className="fab fa-github-square"></i></a>
+        <a href="https://www.linkedin.com/in/dongjinxu"><i className="fab fa-linkedin"></i></a>
         <li className="copyright">© 2018 Dongjin Jill Xu</li>
       </ul>
     );
